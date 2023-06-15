98c9557b130653e473ffe7bd910e87d7c1c4e08f
Rajat Nai <rajatnai49@gmail.com>
2023-06-15 19:06:37 +0530

    Removing damaged files

--- flight/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Flight, fSeat, fBooking
-
-
-admin.site.register(Flight)
-admin.site.register(fSeat)
-admin.site.register(fBooking)
-
--- tours/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Destination, Tour, TourBooking, Comment
-
-admin.site.register(Destination)
-admin.site.register(Tour)
-admin.site.register(TourBooking)
-admin.site.register(Comment)
